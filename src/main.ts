ad8784df1b23304e4314e7871295031574a76706
Rafael Neves <rafael.fne@gmail.com>
2026-08-25 13:37:10 -0300

    Refactor code structure for improved readability and maintainability

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp as createVueApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { ViteSSG } from 'vite-ssg'
-import router from './router'
-
-export const createApp = ViteSSG(App, router, ({ app, isClient }) => {
-})
-
